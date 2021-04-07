9c9c1bec7e95f09a828fdf94020722c09e101016
liubo0127 <liubo0127@qq.com>
2021-04-07 15:43:49 +0800

    Add `SliceSumAll` to calculate multi slice's sum value

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func ArraySum(numbers [5]int) int {
 	sum := 0
@@ -19,10 +22,23 @@ func SliceSum(numbers []int) int {
 	return sum
 }
 
+func SliceSumAll(numbers ...[]int) []int {
+	r := make([]int, 0)
+	for _, s := range numbers {
+		r = append(r, SliceSum(s))
+	}
+	return r
+}
+
 func main() {
 	a := [5]int{1, 2, 3, 4, 5}
 	b := []int{1, 2, 3, 4, 5}
 	r1 := ArraySum(a)
 	r2 := SliceSum(b)
 	fmt.Println(r1, r2)
+
+	c := []int{1, 2, 3}
+	d := []int{4, 5, 6}
+	r3 := SliceSumAll(c, d)
+	fmt.Println(r3)
 }
--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestArraySum(t *testing.T) {
 	type args struct {
@@ -37,6 +40,26 @@ func TestSliceSum(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := SliceSum(tt.args.numbers); got != tt.want {
 				t.Errorf("SliceSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceSumAll(t *testing.T) {
+	type args struct {
+		numbers [][]int
+	}
+	tests := []struct{
+		name string
+		args args
+		want []int
+	}{
+		{"SliceSumAll test1", args{[][]int{{1,2,3}, {4,5,6}}}, []int{6, 15}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceSumAll(tt.args.numbers...); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SliceNumAll() = %v, want %v", got, tt.want)
 			}
 		})
 	}
