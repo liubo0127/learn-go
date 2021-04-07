d899c2203a6f25ebe2b02ff0ccc073a7c244e400
liubo0127 <liubo0127@qq.com>
2021-04-07 15:25:43 +0800

    Add slice sum function and test

--- array_slice/sum.go
+++ array_slice/sum.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func Sum(numbers [5]int) int {
+func ArraySum(numbers [5]int) int {
 	sum := 0
 
 	for _, i := range numbers {
@@ -11,7 +11,18 @@ func Sum(numbers [5]int) int {
 	return sum
 }
 
+func SliceSum(numbers []int) int {
+	sum := 0
+	for _, i := range numbers {
+		sum += i
+	}
+	return sum
+}
+
 func main() {
-	r := Sum([5]int{1, 2, 3, 4, 5})
-	fmt.Println(r)
+	a := [5]int{1, 2, 3, 4, 5}
+	b := []int{1, 2, 3, 4, 5}
+	r1 := ArraySum(a)
+	r2 := SliceSum(b)
+	fmt.Println(r1, r2)
 }
--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestArraySum(t *testing.T) {
 	type args struct {
 		numbers [5]int
 	}
@@ -11,12 +11,32 @@ func TestSum(t *testing.T) {
 		args args
 		want int
 	}{
-		{"sum test1", args{[5]int{1,2,3,4,5}}, 15},
+		{"Array test1", args{[5]int{1,2,3,4,5}}, 15},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Sum(tt.args.numbers); got != tt.want {
-				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			if got := ArraySum(tt.args.numbers); got != tt.want {
+				t.Errorf("ArraySum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSliceSum(t *testing.T) {
+	type args struct {
+		numbers []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Slice test1", args{[]int{1,2,3}}, 6},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SliceSum(tt.args.numbers); got != tt.want {
+				t.Errorf("SliceSum() = %v, want %v", got, tt.want)
 			}
 		})
 	}
